277dfb6fed4194f85522ec11f515407c152a7026
i3ucky <85403339+i3ucky@users.noreply.github.com>
2025-07-29 11:50:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'mysql',
   port: DB_PORT || 3306,
-  logging: console.log
 });
 
 // DB-Modell definieren
